21488a6f7a7e10135e3380bc663f8be58a17688f
Lderz <isaaclander20@gmail.com>
2023-03-17 14:21:55 -0700

    0-positive_or_negative.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that print lines with put function and returns 0
+ * Description: main - A program that print lines with put function 
  * return: always 0 (success)
  */
 int main(void)
