db21320cd634359c5cf0480eae87cb57e5e787b4
Lderz <isaaclander20@gmail.com>
2023-03-17 16:59:34 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,7 +3,7 @@
 /**
  * main - Prints all possible combinations of single-digit numbers.
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
@@ -22,4 +22,3 @@ int main(void)
 
 	return (0);
 }
-
