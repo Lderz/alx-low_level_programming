4c90206178404391a86b923fa643816547869fcb
Lderz <isaaclander20@gmail.com>
2023-04-15 16:54:39 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 
 /**
- * main -  display name
- *@argc: integers
- *@argv: char.
- * Return: Always (0).
+ * main - shows the number of arg passed into it
+ * @argc: The num. of command lines arg.
+ * @argv: array 
+ * Return: always 0
  */
-int main(int argc, __attribute__((unused)) char *argv[])
+
+int main(int argc, char *argv[])
 {
-	printf("%d \n", argc - 1);
+	int a;
+
+	if (argc == 1)
+		printf("%d \n", argc - 1);
+	else
+	{
+		for (a = 0; *argv; a++, argv++)
+			;
+
+		printf("%d \n", a - 1);
+	}
 
 	return (0);
 }
+
